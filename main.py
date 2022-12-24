26359aec88e09262bbc18bba345fed0a3973202f
Michael <xsxsd97@gmail.com>
2022-12-25 02:19:23 +0500

    git commit

--- main.py
+++ main.py
@@ -17,6 +17,15 @@ class WindowManager(ScreenManager):
     pass
 
 
+def display_user_tokens(self):
+    self.root.ids.the_label.text = "local_id: " + self.local_id + "\n user_idToken: " + self.user_idToken
+
+
+def sign_out(self):
+    self.root.ids.firebase_login_screen.log_out()
+    self.root.current = 'firebase_login_screen'
+
+
 class MainApp(MDApp):
     def build(self):
         self.theme_cls.theme_style = "Dark"
